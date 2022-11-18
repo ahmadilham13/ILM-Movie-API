b65b06336bbc94d1a96554c49a1abeccf2604f7a
ahmad ilham <ahmad13ilham13@gmail.com>
2022-11-18 16:39:36 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import {getMovieList, searchMovie} from "./api"
+import { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [popularMovies, setPopularMovie] = useState([])
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setPopularMovie(result)
+    })
+  }, [])
+
+  const PopularMovieList = () => {
+    return popularMovies.map((movie, i) => {
+      return (
+        <div className="Movie-wrapper" key={i}>
+        <div className="Movie-title">{movie.title}</div>
+        <img className="Movie-image" src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`} />
+        <div className="Movie-date">release : {movie.release_date}</div>
+        <div className="Movie-rate">{movie.vote_average}</div>
+      </div>
+      )
+    })
+  }
+  
+  const search = async(q) => {
+    if(q.length > 3) {
+      const query = await searchMovie(q)
+      setPopularMovie(query.results)
+    }
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>ILM Movie API</h1>
+          <input 
+          placeholder='Cari Film' 
+          className='Movie-search' 
+          onChange={ ({target}) => search(target.value) }
+          />
+        <div className="Movie-container">
+          <PopularMovieList />
+        </div>
       </header>
     </div>
   );
